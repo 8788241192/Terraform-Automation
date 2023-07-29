fa06b281173a71c6ee4464e8f4898fa5c042a933
8788241192 <127922270+8788241192@users.noreply.github.com>
2023-07-29 19:25:32 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mydev-tf-state-bucket-10"
     key = "LockID"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-dynamodb-table-1"
   }
 }

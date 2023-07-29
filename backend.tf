e9757c48f7ea0d24b8d199fa7cd429e2c53f8092
8788241192 <127922270+8788241192@users.noreply.github.com>
2023-07-29 20:43:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,22 @@
 terraform {
   backend "s3" {
     bucket = "mydev-tf-state-bucket-10"
-    key = "LockID"
+    key = "terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table-1"
-    terraform plan -lock=false 
-
   }
+}
+
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "terraform_state"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "mydev-tf-state-bucket-10"
+    }
 }
